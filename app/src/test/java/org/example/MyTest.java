3b9d5b4e95990001485755a4a5cb8c8c1be3eabf
kgulbarg <kgulbarg@asu.edu>
2025-04-01 15:30:53 -0700

    Dummy tests

---
+++ app/src/test/java/org/example/MyTest.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class MyTest {
+    @Test public void appHasAGreeting() {
+        assertEquals(0.0, 0.0);
+    }
+}
