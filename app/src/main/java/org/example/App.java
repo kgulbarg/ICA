cc2a4bbd3afb1822bb28a9c59c90c1e1c8b8d11f
kgulbarg <kgulbarg@asu.edu>
2025-04-01 19:29:41 -0700

    Push on test to trigger test-job

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -5,7 +5,7 @@ package org.example;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello Test World!";
     }
 
     public static void main(String[] args) {
